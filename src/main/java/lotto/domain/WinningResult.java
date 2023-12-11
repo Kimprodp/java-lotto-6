015667d86472881d776393aa1d6ddc69c61a1d39
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-11 18:04:26 +0900

    feat(WinningResult): 당첨금과 수익률을 계산하는 기능 추가

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -5,13 +5,38 @@ import lotto.domain.constants.WinningRank;
 
 public class WinningResult {
 
+    private static final int PERCENTAGE = 100;
+
     private final LinkedHashMap<WinningRank, Integer> result;
+    private int winningAmount;
+    private double rateOfReturn;
 
     public WinningResult(LinkedHashMap<WinningRank, Integer> result) {
         this.result = result;
+        calculateWinningAmount();
+        calculateRateOfReturn();
     }
 
     public LinkedHashMap<WinningRank, Integer> getResult() {
         return result;
+    }
+
+    public int getWinningAmount() {
+        return winningAmount;
+    }
+
+    public double getRateOfReturn() {
+        return rateOfReturn;
+    }
+
+    private void calculateWinningAmount() {
+        for (WinningRank rank : result.keySet()) {
+            winningAmount += rank.getWinningAmount() * result.get(rank);
+        }
+    }
+
+    private void calculateRateOfReturn() {
+        int purchaseAmount = result.size() * LottoMachine.PRICE;
+        rateOfReturn = (double) winningAmount / purchaseAmount * PERCENTAGE;
     }
 }
